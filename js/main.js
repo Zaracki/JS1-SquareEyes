0c9d390b35ace56265e17afcd269d782f16ed996
Zaracki <119800248+Zaracki@users.noreply.github.com>
2023-04-26 10:54:22 +0200

    testing

--- js/main.js
+++ js/main.js
@@ -1,27 +1,30 @@
- /** const apiURL = "https://api.noroff.dev/api/v1/square-eyes";
+/** const apiUrl = "https://api.noroff.dev/api/v1/square-eyes";
+const movieDesc =document.getElementById("movie-description");
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(responseJson => {
 
-async function getData(url) {
-  console.log("getData called", url);
+    for (let {description} of responseJson) {
+      console.log(description)
+      const decFact = document.createElement("p");
+      decFact.innerText = description;
+      movieDesc.append(decFact);
+    }
+  }) **/
 
-  try {
-    const response = await fetch(url);
+
+  const container = document.querySelector(".container");
+  const apiUrl = "https://api.noroff.dev/api/v1/square-eyes/352ba432-5b5d-4ccc-9aba-f2704c500cf3";
+
+  async function fetchMovie() {
+    const response = await fetch(apiUrl);
     const json = await response.json();
+
     console.log(json);
-  } catch (error) {
 
+    container.innerHTML = `<h1>${json.title}</h1>
+                             <img src="${json.image}"> 
+                             <div class="details-description">${json.description}</div>`;
   }
-} **/
 
-const url = "https://api.noroff.dev/api/v1/square-eyes";
-
-async function getData(){
-
-        const response = await fetch(url);
-
-        const data = await response.json();
-
-        console.log(data);
-
-}
-
-getData()
+  fetchMovie()
